20646532c15736afc5eb6e6efae2e9bc27c3ce26
MS269 <ehddnr9797@gmail.com>
2022-01-23 23:16:05 +0900

    Style NavBar with CSS Module

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,18 +1,34 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
+  const router = useRouter();
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link href="/">
-            <a>Home</a>
+            <a
+              className={`${styles.link} ${
+                router.pathname === "/" ? styles.active : ""
+              }`}
+            >
+              Home
+            </a>
           </Link>
         </li>
 
         <li>
           <Link href="/about">
-            <a>About</a>
+            <a
+              className={[
+                styles.link,
+                router.pathname === "/about" ? styles.active : "",
+              ].join(" ")}
+            >
+              About
+            </a>
           </Link>
         </li>
       </ul>
