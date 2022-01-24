6f28cc816917c433234d021fd4352ddea3ee857e
MS269 <ehddnr9797@gmail.com>
2022-01-24 21:15:18 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home({
 
       {results?.map((movie) => (
         <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
@@ -26,6 +26,10 @@ export default function Home({
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+
+        .movie {
+          cursor: pointer;
         }
 
         .movie img {
