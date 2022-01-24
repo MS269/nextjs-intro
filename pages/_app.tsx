0ad575055b598a972a43cd07f32c4d59717b9db3
MS269 <ehddnr9797@gmail.com>
2022-01-24 21:10:47 +0900

    Commit Next Movies Website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,11 @@
-import type { AppProps } from "next/app";
-import NavBar from "../components/NavBar";
+import { AppProps } from "next/app";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </>
+    </Layout>
   );
 }
